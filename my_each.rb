997199ad4494b7219ed1c2a2c9563547429af6ef
Dalton Fant <daltonfant16@gmail.com>
2018-02-08 22:14:07 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -1,6 +1,6 @@
 def my_each(array)
   words_in_array = array.length
-  words_returned = 0
+  words_returned = 4
   while words_returned < words_in_array
     array.collect do |word|
     return word
